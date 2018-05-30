c510c24fc541322854f51c3140c0b087c42f9d60
Daniel Ridgway <ridginald@gmail.com>
2018-05-30 16:27:24 +0100

    test helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Dan'
-    fill_in :player_2_name, with: 'Mick'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Dan vs. Mick'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'Player 1 can see Player 2 hit points' do
   scenario 'shows hit points' do
-    visit('/')
-    fill_in :player_1_name, with: "Dan"
-    fill_in :player_2_name, with: "Mick"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Mick: 60hp'
   end
 end
