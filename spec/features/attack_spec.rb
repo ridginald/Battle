4b8892ca0608c827a881ca984d27e0ee33d68ce0
Daniel Ridgway <ridginald@gmail.com>
2018-05-31 15:01:05 +0100

    implementing hit points

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,11 @@ feature 'attacking' do
     click_link 'Attack'
     expect(page).to have_content "Dan attacked Mick"
   end
+
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_link 'Attack'
+    expect(page).not_to have_content 'Mick: 60HP'
+    expect(page).to have_content 'Mick: 50HP'
+  end
 end
--- spec/features/player_spec.rb
+++ spec/features/player_spec.rb
@@ -1,11 +1,31 @@
 require 'player'
 
 describe Player do
-  subject(:Dan) { Player.new('Dan') }
+  subject(:dan) { Player.new('Dan') }
+  subject(:mick) { Player.new('Mick') }
 
   describe '#name' do
     it 'returns the name' do
-      expect(name).to eq 'Dan'
+      expect(dan.name).to eq 'Dan'
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(dan.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(mick).to receive(:receive_damage)
+      dan.attack(mick)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces he player HP' do
+      expect { dan.receive_damage }.to change { dan.hit_points }.by(-10)
     end
   end
 end
